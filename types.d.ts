9a56994597b4f25283741009f82094e457f6517d
Bruce Anderson <anderson.bruce.b@gmail.com>
2023-02-26 15:56:52 -0500

    Initial commit

--- types.d.ts
+++ types.d.ts
@@ -1,8 +1,12 @@
 import {BeDecoratedProps, MinimalProxy} from 'be-decorated/types';
-import {AffectOptions, JSONObject, camelQry, Scope, QueryInfo} from 'trans-render/lib/types';
+import {AffectOptions, JSONObject, camelQry, Scope, QueryInfo, Matches} from 'trans-render/lib/types';
 
 export interface EndUserProps {
     camelConfig?: CamelConfig;
+}
+
+export interface VirtualProps extends EndUserProps, MinimalProxy{
+
 }
 
 export type observingRealm = string;
@@ -11,6 +15,8 @@ export type homeInOnPath = string;
 
 export type sharingRealm = string;
 
+export type propName = string;
+
 export type SetStatement = 
     |   `ObservingRealmTo${observingRealm}` 
     |   `HomeInOnPathTo${homeInOnPath}`
@@ -18,6 +24,7 @@ export type SetStatement =
 
 export interface CamelConfig {
     Set: SetStatement[],
-    Share?: `${string}To${camelQry}As${string}`[]
+    Share?: `${string}To${camelQry}As${string}`[],
+    [key: `share${propName}To`]: Matches
 }
 
